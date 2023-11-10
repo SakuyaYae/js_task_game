73a22dcde9915eb2484691dd233495bc41b4a976
SakuyaYae <darkreimu@outlook.com>
2023-11-10 15:27:56 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,10 +34,21 @@ class GameLogic {
   static #playerTurnKeeper = 1;
   static #player1UnitCount = 0;
   static #player2UnitCount = 0;
-  static #GameRoundCount = 0;
+  static #GameRoundCount = 1;
+  static #player1 = {
+    userName: "Vayle",
+    score: "0",
+    matchHistory: []
+  };
+  static #player2 = {
+    userName: "Saya",
+    score: "0",
+    matchHistory: []
+  };
 
   static gameStart() {
     this.#playerTurnDisplay();
+    this.#getPlayerData();
   }
 
   static playerMove(input) {
@@ -45,7 +56,9 @@ class GameLogic {
       this.#playerUnitUpdate(input);
       this.#playerTurnUpdate();
       this.#playerTurnDisplay();
-      this.#winCheck();
+      if (this.#winCheck()) {
+        this.#createMatchHistory();
+      }
     }
     else {
       console.log("Invalid move")
@@ -66,6 +79,9 @@ class GameLogic {
   }
   static #moveValidation(input) {
     var valid = true;
+    if (input.innerText == "X" || input.innerText == "O") {
+      valid = false;
+    }
     return valid;
   }
 
@@ -106,8 +122,54 @@ class GameLogic {
   }
 
   static #winCheck() {
-
+    var gameOver = false;
+    return gameOver;
   }
+
+  static #createMatchHistory() {
+    if (this.#GameRoundCount % 2 === 1) {
+      const matchWonP1 = {
+        opponent: this.#player2.userName,
+        result: "Win"
+      }
+      const matchLostP2 = {
+        opponent: this.#player1.userName,
+        result: "Lost"
+      }
+      this.#player1.matchHistory.push(matchWonP1);
+      this.#player1.score += 3;
+      this.#player2.matchHistory.push(matchLostP2);
+      this.#player2.score += 1;
+    }
+    else {
+      const matchWonP2 = {
+        opponent: this.#player1.userName,
+        result: "Win"
+      }
+      const matchLostP1 = {
+        opponent: this.#player2.userName,
+        result: "Lost"
+      }
+      this.#player1.matchHistory.push(matchWonP2);
+      this.#player1.score += 1;
+      this.#player2.matchHistory.push(matchLostP1);
+      this.#player2.score += 3;
+    }
+  }
+  static #getPlayerData() {
+    const userData = getJsonData();
+  }
+}
+
+async function getJsonData() {
+  const requestURL = "./json/users.json"
+  const request = new Request(requestURL, {
+    mode: "no-cors"
+  });
+  const response = await fetch(request);
+  const userData = response;
+  console.log(userData);
+  return userData;
 }
 
 function main() {
